fb0e03df0a1ce3dfc0edcbb750c1c783679a7d02
beebekisme <021a306@sxc.edu.np>
2024-02-07 16:05:56 +0545

    vc

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,14 +10,9 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'monokai',
       langs: ['python'],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
-      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
-      // Find common transformers: https://shikiji.netlify.app/packages/transformers
       transformers: []
     }
   },
