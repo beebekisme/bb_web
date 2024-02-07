0cc2250cb4aadbeb1824dda0b54fa2f0745a0f38
beebekisme <021a306@sxc.edu.np>
2024-02-07 15:55:18 +0545

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,25 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://beebekisme.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'monokai',
-
       langs: ['python'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true,
       // Add custom transformers: https://shikiji.netlify.app/guide/transformers
       // Find common transformers: https://shikiji.netlify.app/packages/transformers
-      transformers: [],
-    },
+      transformers: []
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
